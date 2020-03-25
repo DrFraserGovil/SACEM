c4bfe27fe921e170db55004ca87b608ffa916c63
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2020-03-25 11:01:08 +0000

    Stupid error in oversave mechanism has been fixed

--- src.cpp
+++ src.cpp
@@ -69,7 +69,7 @@ inline bool evaluateSaveConditions(ParameterPack state, int i, int j)
 		}
 		else
 		{
-			return  (rand() % (state.SaveValue/3) == 0);
+			return  (rand() % (state.SaveValue/2) == 0);
 		}
 	}
 }
