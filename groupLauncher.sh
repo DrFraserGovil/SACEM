3de54ba7021ed296061a36b531d219f6a32a8dcc
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2020-09-03 17:00:45 +0100

    More reasonable launcher

--- groupLauncher.sh
+++ groupLauncher.sh
@@ -1,31 +1,19 @@
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelM_All -mode 1 -dt 0.2 -constrain m -allfrac 1 -gradient 0
- sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelM_Cold -mode 1 -dt 0.2 -constrain m -allfrac 0 -gradient 0
-  sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelL_All -mode 1 -dt 0.2 -constrain l -allfrac 1 -gradient 0
-  sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelL_Cold -mode 1 -dt 0.2 -constrain l -allfrac 0 -gradient 0
-  sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelT_All -mode 1 -dt 0.2 -constrain t -allfrac 1 -gradient 0
- sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelT_Cold -mode 1 -dt 0.2 -constrain t -allfrac 0 -gradient 0
- sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelS_All -mode 1 -dt 0.2 -constrain s -allfrac 1 -gradient 0
-  sleep 1
- ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelS_Cold -mode 1 -dt 0.2 -constrain s -allfrac 0 -gradient 0
-  sleep 1.
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelM_All_Grad -mode 1 -dt 0.2 -constrain m -allfrac 1 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelM_Cold_Grad -mode 1 -dt 0.2 -constrain m -allfrac 0 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelL_All_Grad -mode 1 -dt 0.2 -constrain l -allfrac 1 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelL_Cold_Grad -mode 1 -dt 0.2 -constrain l -allfrac 0 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelT_All_Grad -mode 1 -dt 0.2 -constrain t -allfrac 1 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelT_Cold_Grad -mode 1 -dt 0.2 -constrain t -allfrac 0 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelS_All_Grad -mode 1 -dt 0.2 -constrain s -allfrac 1 -gradient 1
-  sleep 1
- ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelS_Cold_Grad -mode 1 -dt 0.2 -constrain s -allfrac 0 -gradient 1
+
+for model in L M T S
+do
+	for fractionMode in 0 1
+	do
+		for gradientMode in 0 1
+		do 
+			base="./submitLong -threads 28"
+			if [ $gradientMode -eq 0 ]
+			then
+				base="./submitShort -threads 16"
+			fi
+			command="${base} -random 10000000 -grid 101 -dir Output/Model${model}_${fractionMode}_${gradientMode} -mode 1 -dt 0.2 -constrain ${model} -allfrac ${fractionMode} -gradient ${gradientMode}"
+			echo $command
+		done
+		
+	done
+done
+ #./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelS_Cold_Grad -mode 1 -dt 0.2 -constrain s -allfrac 0 -gradient 1
