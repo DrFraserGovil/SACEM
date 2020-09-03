043c04b4c6859967c49a2d24555570120c979873
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2020-09-03 16:46:15 +0100

    Attempt to mollify issue with launch

--- groupLauncher.sh
+++ groupLauncher.sh
@@ -14,7 +14,7 @@
   sleep 1
  ./submitShort -threads 16 -random 10000000 -grid 101 -dir Output/ModelS_Cold -mode 1 -dt 0.2 -constrain s -allfrac 0 -gradient 0
   sleep 1.
- /submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelM_All_Grad -mode 1 -dt 0.2 -constrain m -allfrac 1 -gradient 1
+ ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelM_All_Grad -mode 1 -dt 0.2 -constrain m -allfrac 1 -gradient 1
   sleep 1
  ./submitLong -threads 28 -random 10000000 -grid 101 -dir Output/ModelM_Cold_Grad -mode 1 -dt 0.2 -constrain m -allfrac 0 -gradient 1
   sleep 1
